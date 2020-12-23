a3935ca2b7244cf5e32eb80b74dac4ef89e39c13
pirboazo <pib@pirboazo.net>
2020-12-23 15:39:13 +0100

    Restructuration du site

--- script/agate.bash
+++ script/agate.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 # lancement de agate 
 # 
-#agate 0.0.0.0:1965 /home/pboizot/www/gemini/ cert.pem key.rsa
-agate 2a04:ee41:84:d32d:e219:204f:5103:21:1965 /home/pi/www/gemini/ cert.pem key.rsa
+agate --addr 0.0.0.0:1965 --content /home/pi/www/gemini/ cert.pem key.rsa
+agate --addr 2a04:ee41:84:d32d:e219:204f:5103:21:1965 --content /home/pi/www/gemini/ cert.pem key.rsa
